2638b92f2dff44397cc18c6394813e6715196f6b
Stéphanie Favre <stephaniefavre@MacBook-Pro-de-Stephanie-4.local>
2017-12-10 21:01:13 +0100

    ISO 19139 Template Generator

--- export_xml_metadata.py
+++ export_xml_metadata.py
@@ -1,10 +1,11 @@
+# -*-coding:Latin-1 -*
 # Version: 	1.0.0
 # Author: 	Stephanie Favre
 #			University of Geneva
 #			Institute for Environmental Sciences/enviroSPACE
 #			Uni Carl-Vogt
 #			66 boulevard Carl-Vogt
-#			CH - 1205 GenÃ¨ve
+#			CH - 1205 Genève
 #			http://www.unige.ch/envirospace/people/stephanie-favre/
 #			http://www.unige.ch/envirospace
 #
@@ -22,57 +23,13 @@
 
 
 import psycopg2.extras
-import xml.etree.cElementTree as ET
-from xml.dom import minidom
 import os
+from jinja2 import Template,Environment,FileSystemLoader
+import random
+from datetime import date
+import uuid
+from time import gmtime, strftime
 
-
-# Method to get a list of all attributes with table name, attribute, data type, description (theme set value to 'none')
-def get_attribute_type_desc(
-        cur
-):
-    metadata_list = []
-    querry = """
-        SELECT table_name, attribute, data_type, description FROM metadata.tmeta_global
-    """
-    cur.execute(querry)
-
-    for elements in cur.fetchall():
-        metadata_obj = {
-            "theme": 'none',
-            "table_name": elements['table_name'],
-            "attribute": elements['attribute'],
-            "data_type": elements['data_type'],
-            "description": elements['description']
-        }
-        metadata_list.append(metadata_obj)
-
-    return metadata_list
-
-
-# Method to add theme values to metadata list from table tmeta_theme
-def add_theme_to_metadatalist(
-    cur,
-    metadata_list
-    ):
-
-    for metadata_obj in metadata_list :
-        theme_list = []
-        querry = """
-            SELECT theme FROM metadata.tmeta_theme where table_name='{table_name}'
-        """.format(table_name=metadata_obj['table_name'])
-        cur.execute(querry)
-        for themes in cur.fetchall():
-            for element in themes:
-                theme_list.append(element)
-
-        metadata_obj.update(
-                 {
-                     "theme" : theme_list
-                 }
-        )
-
-    return metadata_list
 
 # Method to get distinct themes from database
 def get_themes(
@@ -90,12 +47,41 @@ def get_themes(
 
     return theme_list
 
-# Method to get a human readable XML file
-def prettify(elem):
 
-    rough_string = ET.tostring(elem, 'utf-8')
-    reparsed = minidom.parseString(rough_string)
-    return reparsed.toprettyxml(indent="  ")
+
+
+
+# Method to generate XML from Template
+def iso19139_generator(attributes_list,
+                       theme,
+                       template_path):
+
+    random_id = random.getrandbits(128)
+
+    templateloader = FileSystemLoader(searchpath=template_path)
+    templateenv = Environment(loader=templateloader,trim_blocks=True)
+    TEMPLATE_FILE = "sitg_iso_19139.xml"
+    template = templateenv.get_template(TEMPLATE_FILE)
+    rendered = template.render(attributes_list=attributes_list,
+                               theme=theme,
+                               random_id=random_id,
+                               today_date=date.today(),
+                               today_date_time=strftime("%Y-%m-%dT%H:%M:%S", gmtime()),
+                               uuid=uuid.uuid4(),
+                               today_separate=strftime("%Y-%m-%d", gmtime())
+    )
+    return rendered
+
+
+# Define Object Attribute (Description, name..)
+class Attribute(object):
+    def __init__(self, theme, name, table_name, data_type, description):
+        self.theme = theme
+        self.name = name
+        self.table_name = table_name
+        self.data_type = data_type
+        self.description = description
+
 
 
 if __name__ == '__main__':
@@ -116,30 +102,13 @@ if __name__ == '__main__':
     conn = psycopg2.connect(dbname=db, user="postgres", password="123", host="127.0.0.1")
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    # Get all elements (theme, table name, attribute, data type, description) from the metadata table -> Create metadata list
-    metadata_list = []
-    metadata_list = get_attribute_type_desc(cur=cur
-    )
 
-    # Add theme value related to the metadata list
-    metadata_list = add_theme_to_metadatalist(
-        cur=cur,
-        metadata_list=metadata_list
-    )
+    attributes_list = []
 
-
-    # Get list of all themes
+    # Get Theme List
     theme_list = get_themes(cur)
 
-    # Iterate on each theme
-    # Start implementing XML parts
     for theme_element in theme_list:
-
-        metadata = ET.Element('metadata')
-
-        theme = ET.SubElement(metadata, 'theme')
-        theme.text = theme_element
-
         # Get all tables related to the selected theme
         querry = """
         SELECT table_name FROM metadata.tmeta_theme  WHERE theme='{theme_element}'
@@ -147,48 +116,42 @@ if __name__ == '__main__':
             theme_element=theme_element
         )
         cur.execute(querry)
-
         tables_list = []
         for elements in cur.fetchall():
             for table in elements:
-                tables_list.append(table)
+                # Get all attributes related to the selected table
+                querry = """
+                    SELECT table_name,attribute,data_type,description  FROM metadata.tmeta_global WHERE table_name='{table}'
+                """.format(
+                    table=table
+                )
 
-        # Create object 'Table name'
-        for table in tables_list:
-            table_name = ET.SubElement(theme, 'table_name')
-            table_name.text = table
-
-
-            # Get all attributes related to the selected table
-            querry = """
-                SELECT table_name,attribute,data_type,description  FROM metadata.tmeta_global WHERE table_name='{table}'
-            """.format(
-                table=table
-            )
-
-            # Get data type and description for each attribute
-            cur.execute(querry)
-            for elements in cur.fetchall():
-
-                attribute = ET.SubElement(table_name, 'attribute')
-                attribute.text = elements['attribute']
-
-                data_type = ET.SubElement(attribute, 'data_type')
-                data_type.text = elements['data_type']
-
-                description = ET.SubElement(attribute, 'description')
-                description.text = elements['description']
+                # Get data type and description for each attribute
+                cur.execute(querry)
+                for elements in cur.fetchall():
+                    attributes_list.append(
+                        Attribute(
+                            theme=theme_element,
+                            name=elements['attribute'],
+                            table_name=table,
+                            data_type=elements['data_type'],
+                            description=elements['description']
+                        )
+                    )
+        #Generate Template for each theme
+        xml_generated = iso19139_generator(attributes_list=attributes_list,
+                           theme=theme_element,
+                           template_path='../source-files/templates/')
 
         # Create an XML file for each theme
         with open('xml/{theme_element}.xml'.format(
-                theme_element=theme_element), 'w') as file:
-            file.write(prettify(metadata))
+            theme_element=theme_element), 'w') as file:
+            file.write(xml_generated)
+
 
         print("XML File created for {theme_element}".format(
             theme_element=theme_element
         ))
 
-    print("All metadata assembled in XML Object")
-
-
+        print("All metadata assembled in XML Object")
 
